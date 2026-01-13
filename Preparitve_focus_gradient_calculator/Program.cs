0eb8771d5fc265f8a30156dcc00b720fff93b8fc
Zachary St.Cyr <2583885@johnabbottcollege.net>
2026-01-13 16:49:45 -0500

    made html page

--- Preparitve_focus_gradient_calculator/Program.cs
+++ Preparitve_focus_gradient_calculator/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            int sum = 0;
+            int walkupnum;
+            //user walkup instrument
+            Console.WriteLine("which walkup instrument did you use?");
+            walkupnum = Convert.ToInt32(Console.ReadLine());
+            switch (walkupnum)
+            {
+                case 1:
+                case 2: highnumberfour(); break;
+                case 3:
+                case 4:
+                case 5:
+                case 6: highnumberfour(); break;
+            }
+            static void highnumberfour()
+            {
+                double time;
+                bool inbound = true;
+                double Gradient_Center;
+                double M = 27.137;
+                double B = -31.741;
+                Console.Write("Retention time (min)");
+                do
+                {
+                    time = Convert.ToInt32(Console.ReadLine());
+                    if (time >= 0 || time > 4.7)
+                    {
+                        inbound = false;
+                    }
+                    else
+                    {
+                        inbound = true;
+                    }
+                }
+                while (inbound == false);
+                Gradient_Center = M * (time) + B;
+            }
         }
     }
 }
